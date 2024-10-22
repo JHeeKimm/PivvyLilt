d33f0c72b543c6f3a32247a97983554ea22af0fc
JheeKim <jheekim992@gmail.com>
2024-10-22 23:45:30 +0900

    Feat: 회원가입 유효성 검사 조건 수정

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -2,5 +2,9 @@ type Props = {
   message: string;
 };
 export function ErrorMessage({ message }: Props) {
-  return <p className="text-sm text-red-600 ml-1 mt-1">{message}</p>;
+  return (
+    <p className="max-w-xs break-words text-sm text-red-600 ml-1 mt-1">
+      {message}
+    </p>
+  );
 }
--- src/lib/auth/schema.ts
+++ src/lib/auth/schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+const easyPatterns = ["123456", "password", "qwerty", "asdf", "admin"];
+
 export const SignUpSchema = z.object({
   nickName: z
     .string()
@@ -11,18 +13,33 @@ export const SignUpSchema = z.object({
   password: z
     .string()
     .min(8, { message: "비밀번호는 최소 8자 이상이어야 합니다." })
-    .regex(/[A-Z]/, {
-      message: "비밀번호는 최소 1개 이상의 대문자를 포함해야 합니다.",
-    })
-    .regex(/[a-z]/, {
-      message: "비밀번호는 최소 1개 이상의 소문자를 포함해야 합니다.",
+    .regex(/[a-zA-Z]/, {
+      message: "비밀번호는 최소 1개 이상의 영문자를 포함해야 합니다.",
     })
     .regex(/[0-9]/, {
       message: "비밀번호는 최소 1개 이상의 숫자를 포함해야 합니다.",
     })
     .regex(/[\W_]/, {
       message: "비밀번호는 최소 1개 이상의 특수문자를 포함해야 합니다.",
-    }),
+    })
+    .refine(
+      (password) => !easyPatterns.some((pattern) => password.includes(pattern)),
+      {
+        message:
+          "비밀번호에 쉬운 패턴(예: '123456', 'qwerty')이 포함되어 있습니다.",
+      }
+    )
+    .refine(
+      (password) => !/\d{3,}/.test(password), // 세 자리 이상의 연속된 숫자 방지
+      { message: "비밀번호에 연속된 숫자나 전화번호가 포함되어 있습니다." }
+    )
+    .refine(
+      (password) => !/([a-zA-Z])\1{2,}/.test(password), // 동일한 문자가 3번 이상 연속되는 패턴 방지
+      {
+        message:
+          "비밀번호에 동일한 문자가 연속해서 3번 이상 포함되어 있습니다.",
+      }
+    ),
 });
 
 export const LoginSchema = z.object({
