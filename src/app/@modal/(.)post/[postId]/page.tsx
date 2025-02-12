c733819c50bbbbd51b06d566414d1920ed02b140
JheeKim <jheekim992@gmail.com>
2025-02-12 17:02:55 +0900

    Refactor: Modal 개선
    - 개선 이유:
      - 페이지 이동 없이 모달을 url 상태로 관리할 수 있는 Intercepting Routes로 모달을 구현한 상태
      - 모달이 페이지 트리 구조 내에서 렌더링되므로 createPortal이 불필요하다고 판단
      - React의 createPortal은 모달을 DOM 트리의 최상위로 이동시켜 렌더링함
    
    - 변경 사항:
      - createPortal 삭제
      - dialog 태그 사용 대신 라우터 상태로 모달의 열림/닫힘을 관리

--- src/app/@modal/(.)post/[postId]/page.tsx
+++ src/app/@modal/(.)post/[postId]/page.tsx
@@ -4,10 +4,8 @@ import PostDetailPage from "@/app/(afterLogin)/post/[postId]/page";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Page(props: any) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70">
-      <Modal>
-        <PostDetailPage {...props} />
-      </Modal>
-    </div>
+    <Modal>
+      <PostDetailPage {...props} />
+    </Modal>
   );
 }
